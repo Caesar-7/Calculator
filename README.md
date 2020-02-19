09c6896c97478474f7ed0693331af86dcad8fba5
Caesar-7 <giusemariotti77@gmail.com>
2020-02-19 23:11:09 +0100

    Improved Python code

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Calculator
-A simple calculator in Python, for additions, subtractions, multiplication, divions, powers, square roots, sin, cos and tan.
+A simple calculator in Python. Operations: +, -, ✕, /, square root, sin, cos, and tan.
 # Sintax
-1) Type of result (int, float, roundFloat(a rounded float))
-2) Operator (+, -, *, /, pow, sqrt, sin, cos, tan)
-3) First number
-4) Type of number you want to input, degrees or radians(deg, rad), only if you chose sin, cos or tan
-5) Second number, if you chose an operator that needs two values
-6) Number of digits of the result, only if you chose roundFloat ("unit" if you want
-to round at the unit, or specify how many digits you want)
+1) Type of result (int, float, round(a rounded float))
+2) First number
+3) Operator (+, -, ✕, /, pow, sqrt, sin, cos, tan)
+4) Second number, if you chose an operator that needs two values
+5) Number of digits of the result, only if you chose roundFloat
--- calculator.py
+++ calculator.py
@@ -1,75 +1,47 @@
-# Created by Caesar 2/06/2019
+# Created by Caesar-7 19/02/2020 dd/mm/yy
 
-# Sintax of the input:
-# Type of result (int, float, roundFloat)
-# Operator (+, -, *, /, pow, sqrt, sin, cos, tan)
-# First number
-# Type of input, only if you chose sin, cos or tan (deg, rad)
-# Second number, only if you chose an operator that needs two values
-# Number of digits of the result, only if you chose roundFloat ("unit" if you want
-# to round at the unit, or specify how many digits you want)
+from math import sin,cos,tan,sqrt,radians
 
-import math as mt
-numType = input("Type of result: ")
+op1 = ("sin","cos","tan","sqrt")
+op2 = ("+","-","*","/","**","^")
+
+rtype = input("Type of result: ")
+n1 = float(input("First number: "))
 op = input("Operator: ")
-num1 = float(input("First number: "))
 
-if op == "+":
-    num2 = float(input("Second number: "))
-    r = num1 + num2
-elif op == "-":
-    num2 = float(input("Second number: "))
-    r = num1 - num2
-elif op == "*":
-    num2 = float(input("Second number: "))
-    r = num1 * num2
-elif op == "/":
-    num2 = float(input("Second number: "))
-    r = num1 /num2
-elif op == "pow":
-    num2 = float(input("Second number: "))
-    r = mt.pow(num1, num2)
-elif op == "sqrt":
-    r = mt.sqrt(num1)
-elif op == "sin":
-    inputType = input("Type of input: ")
-    if inputType == "deg":
-        num1 = mt.radians(num1)
-        r = mt.sin(num1)
-    elif inputType == "rad":
-        r = mt.sin(num1)
-    else:
-        print("Not supported")
-elif op == "cos":
-    inputType = input("Type of input: ")
-    if inputType == "deg":
-        num1 = mt.radians(num1)
-        r = mt.cos(num1)
-    elif inputType == "rad":
-        r = mt.cos(num1)
-    else:
-        print("Not supported")
-elif op == "tan":
-    inputType = input("Type of input: ")
-    if inputType == "deg":
-        num1 = mt.radians(num1)
-        r = mt.tan(num1)
-    elif inputType == "rad":
-        r = mt.tan(num1)
-    else:
-        print("Not supported")
+if op in op1:
+	if op == "sin":
+		n1 = radians(n1)
+		r = sin(n1)
+	elif op == "cos":
+		n1 = radians(n1)
+		r = cos(n1)
+	elif op == "tan":
+		n1 = radians(n1)
+		r = tan(n1)
+	elif op == "sqrt":
+		r = sqrt(n1)
+elif op in op2:
+	n2 = float(input("Second number: "))
+	if op == "+":
+		r = n1 + n2
+	elif op == "-":
+		r = n1 - n2
+	elif op == "*":
+		r = n1 * n2
+	elif op == "/":
+		r = n1 / n2
+	elif op == "**" or op == "^":
+		r = n1 ** n2
 else:
-    print("Error")
+	raise ValueError("Operator not supported")
 
-if numType == "int":
-    print(int(r))
-elif numType == "float":
-    print(float(r))
-elif numType == "roundFloat":
-    numRound = input("Round: ")
-    if numRound == "unit":
-        print(round(r))
-    else:
-        print(round(r, int(numRound)))
+if rtype == "int":
+	print(int(r))
+elif rtype == "float":
+	print(r)
+elif rtype == "round":
+	nround = input("Round: ")
+	print(round(r, int(nround)))
 else:
-    print("Not supported")
+	raise ValueError("Type not supported")
